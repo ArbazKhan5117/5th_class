57e97810b148bbba23d2a2bcaedb288d9dc82434
ArbazKhan5117 <arbazkhan5117@gmail.com>
2020-09-25 11:43:51 +0000

    updation

--- src/CountReducer.js
+++ src/CountReducer.js
@@ -1,7 +1,8 @@
 
-export const counterReducer=(state,action)=>{
+const counterReducer=(state,action)=>{
     switch (action) {
         case 'INCREMENT':
             return state + 1;
     }
 }
+export default counterReducer;
